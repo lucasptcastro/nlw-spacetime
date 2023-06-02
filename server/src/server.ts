d0fddf2330e0650b2a84b364dbfa420ad1ed3645
lucasptcastro <lucasptcastro@gmail.com>
2023-06-02 08:20:45 -0300

    ✨ feat: added api routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,18 @@
-import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { memoriesRoutes } from "./routes/memories";
 
 // fastify -> serve para criar rotas HTTPS
-// prisma -> conexão com a api
-const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany() // lista todos os usuários na rota /users
+const app = fastify();
 
-  return users
-})
+app.register(cors, {
+  origin: true, // -> indica que qualquer front-end pode acessar a api (não é indicado)
+  // origin: ["http://localhost:3333", "http://rocketseat.com.br/api"] -> indica qual/quais front-ends podem acessar a api (medida de segurança)
+});
+
+app.register(memoriesRoutes);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('🚀 HTTP server running on http://localhost:3333')
-})
+  console.log("🚀 HTTP server running on http://localhost:3333");
+});
