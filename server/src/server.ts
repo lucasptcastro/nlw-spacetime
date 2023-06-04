71335babbfd0657c1589ac3f363f3cf73008f6e0
lucasptcastro <lucasptcastro@gmail.com>
2023-06-04 19:01:07 -0300

    ✨ feat: added jwt, auth routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,27 @@
+import "dotenv/config";
+
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
 
 // fastify -> serve para criar rotas HTTPS
-
 const app = fastify();
 
+// "origin: true" -> indica que qualquer front-end pode acessar a api (não é indicado)
+// origin: ["http://localhost:3333", "http://rocketseat.com.br/api"] -> indica qual/quais front-ends podem acessar a api (medida de segurança)
 app.register(cors, {
-  origin: true, // -> indica que qualquer front-end pode acessar a api (não é indicado)
-  // origin: ["http://localhost:3333", "http://rocketseat.com.br/api"] -> indica qual/quais front-ends podem acessar a api (medida de segurança)
+  origin: true,
 });
 
+// jwt é uma forma de guardar dados temporários na web
+// "secret" -> é a criptografia do token. é bom colocar caracteres aleatórios. Ex.: "daidashdhashdsanfsdhbfauyosghfaushfsakdjlfbnsaç"
+app.register(jwt, {
+  secret: "spacetime",
+});
+
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app.listen({ port: 3333 }).then(() => {
