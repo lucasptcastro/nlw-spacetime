84ee2aff6279f17deb921a9b33f3cbb3b686fb0d
lucasptcastro <lucasptcastro@gmail.com>
2023-06-05 15:26:28 -0300

    ✨ feat: added uploadRoute, fastify/multipart, fastify/static

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,23 @@ import "dotenv/config";
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "path";
 
 // fastify -> serve para criar rotas HTTPS
 const app = fastify();
+
+app.register(multipart);
+
+// cria rotas estáticas para poder visualizar os arquivos da pasta upload
+// dirname -> caminho em que está o arquivo atual
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+});
 
 // "origin: true" -> indica que qualquer front-end pode acessar a api (não é indicado)
 // origin: ["http://localhost:3333", "http://rocketseat.com.br/api"] -> indica qual/quais front-ends podem acessar a api (medida de segurança)
@@ -22,6 +34,7 @@ app.register(jwt, {
 });
 
 app.register(authRoutes);
+app.register(uploadRoutes);
 app.register(memoriesRoutes);
 
 app.listen({ port: 3333 }).then(() => {
